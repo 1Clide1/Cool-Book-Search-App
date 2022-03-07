a5fdf68c8af6f6a6ba479be5cf0b187139d71623
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-03-07 16:34:37 -0500

    finishing up the resolvers now from it's unfinished state, finished the query me route and now wokring on the mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,38 @@
+// import everything I need for the graphql routes
 const { User } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
 
+// resovlers basically holds all of the api routes
 const resolvers = {
+  // route to get the user
   Query: {
-    me: async (parent, args, context) => {},
+    me: async (parent, args, context) => {
+      // using context to get the user information
+      // basically if user exists then grab the user by its id while adding their saved books
+      if (context.user) {
+        // populate has to be in lowercase and is not case insensitive
+        const userData = await User.findOne({ _id: context.user._id }).populate(
+          "savedbooks"
+        );
+        return userData;
+      }
+      //if not then throw an error
+      throw new AuthenticationError(
+        "You are not logged in, can't find anything!"
+      );
+    },
   },
   Mutation: {
     addUser: async (parent, args, context) => {},
     login: async (parent, { email, password }) => {},
-    saveBook: async (parent, args, context)=>{
-        if (context.user){
-            const updateUser= await
-        }
-    }
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const updateUser = await User;
+      }
+    },
   },
 };
 
+// export resolvers
 module.exports = resolvers;
