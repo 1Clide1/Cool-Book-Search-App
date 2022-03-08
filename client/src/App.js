c52c5cc088408303494039c892c8ea8bbc2a3abd
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-03-07 22:59:37 -0500

    working on setting up graphql with the app.js imported gql added a function to create the link and also to use the authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHTTPLink,
+} from "@apollo/client";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 
 function App() {
+  // creates the link for gql
+  const httplink = createHTTPLink({
+    uri: "/graphql",
+  });
+  // this sets up the json web token to work
+  // save the token to local storage
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem("id_token");
+    return {
+      // return all of the headers and auth if it needs the auth it will take the token otherwise it will do nothing instead
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : ``,
+      },
+    };
+  });
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
