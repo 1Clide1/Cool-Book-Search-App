90c3eba79986a21123371f7f082a9f744e1f73e8
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-03-07 22:30:27 -0500

    added the package-lock.json from the main root also fixed up the server/resolvers because I was going through an issue once that was fixed the resolvers were working just fine

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,6 @@
+// import the type defs and resolvers
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
+// export both of them
 module.exports = { typeDefs, resolvers };
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -25,29 +25,32 @@ const resolvers = {
     },
   },
   // mutations are basically routes that do something other than a get request
+  // learned something new so if you are adding something to a model you need to call it by that models name
+  // with the model user for example if I want to add a user I can't have a const that is named something different to the model so in this case User is already taken so I have to use 'user' which is lowercase
+  // either that or it's because I specifically said in context that it is context.user to say I am getting my info from that
   Mutation: {
     // route to add a user
     addUser: async (parent, args, context) => {
       // creating a user args represent all of the typeDefs in the user model
-      const addUser = await User.create(args);
+      const user = await User.create(args);
       // once a user is created then giving that user a token
-      const token = signToken(addUser);
+      const token = signToken(user);
       // using deconstruction returning the token and user that way it can be properly authenticated
-      return { token, addUser };
+      return { token, user };
     },
     // route to login
     // deconstructing the args to specifically get the email and password from the user model
     login: async (parent, { email, password }) => {
       // first get the user's email
-      const loginUser = await User.findOne({ email });
+      const user = await User.findOne({ email });
       // if user doesn't exist throw err
-      if (!loginUser) {
+      if (!user) {
         throw new AuthenticationError(
           "No users found or user does not exist: wrong credentials(?)"
         );
       }
       // if the user is found then check the password
-      const checkPass = await loginUser.isCorrectPassword(password);
+      const checkPass = await user.isCorrectPassword(password);
       // if password is wrong then throw an error
       if (!checkPass) {
         throw new AuthenticationError(
@@ -55,8 +58,8 @@ const resolvers = {
         );
       }
       // if all is good then create a token for the user that way they can log in
-      const token = signToken(loginUser);
-      return { token, loginUser };
+      const token = signToken(user);
+      return { token, user };
     },
     // route to save the books to a favorite list
     saveBook: async (parent, args, context) => {
